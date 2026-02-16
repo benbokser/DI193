c1b91a753c145babc3929a23ea9c63bbc9435701
Ben Bokser <ben.bokser@gmail.com>
2026-02-16 15:04:02 +0200

    day2.3

--- week1/day2/DailyChallenge/Challenges.py
+++ week1/day2/DailyChallenge/Challenges.py
@@ -1,3 +1,18 @@
+# Instructions:
+# Challenge 1: Multiples of a Number
+# 1. Ask the user for two inputs:
+
+#     A number (integer).
+#     A length (integer).
+number = int(input('Type an integer.'))
+length = int(input('Type another integer for the length of multiples.'))
+# 2. Create a program that generates a list of multiples of the given number.
+# 3. The list should stop when it reaches the length specified by the user.
+list1 = []
+for i in range(1,length+1):
+    list1.append(number * i)
+print(list1)
+
 # Challenge 2: Remove Consecutive Duplicate Letters
 
 
